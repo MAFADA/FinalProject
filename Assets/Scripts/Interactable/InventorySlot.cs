c1f4319f013c8823ad36fbb046f90c2415bd9a73
kevin <73090726+BinaryBenevolent@users.noreply.github.com>
2023-06-02 20:00:30 +0700

    Masih Error

--- Assets/Scripts/Interactable/InventorySlot.cs
+++ Assets/Scripts/Interactable/InventorySlot.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static UnityEditor.Progress;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    public InventoryItem inventoryItem;
+
+    public void ViewDescription()
+    {
+        if(inventoryItem.transform.childCount > 0)
+        {
+            Debug.Log("KLIK");
+            inventoryItem = this.GetComponentInChildren<InventoryItem>();
+            inventoryItem.ViewDescription();
+        }
+        else
+        {
+            Debug.Log("Item gak ada");
+        }
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         if(transform.childCount == 0)
@@ -13,4 +30,6 @@ public class InventorySlot : MonoBehaviour, IDropHandler
             inventoryItem.parentAfterDrag = transform;
         }
     }
+
+    
 }
